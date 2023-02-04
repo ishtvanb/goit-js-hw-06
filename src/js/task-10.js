4aec0dc316487303b2b9757f46a5573a81fa8cc7
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-04 21:22:16 +0300

    Додані зміни до десятого завдання.

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,32 +1,33 @@
 
 const inputBtn = document.querySelector('#controls>input');
-const createBtn = document.querySelector('btn[data-create]');
-const destroyBtn = document.querySelector('btn[data-destroy]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-const createBoxes = (amount) => {
+
+createBtn.addEventListener('click', createBoxes);
+function createBoxes() {
   const arrayBoxes = [];
 
-  for (let i = 1; i <= 100; i += 1) {
-    const box = document.createElement('div');
+  for (let i = 0; i < 100; i += 1) {
+
+    const div = document.createElement('div');
 
     div.style.height = `${i * 30 + 10}px`;
     div.style.width = `${i * 30 + 10}px`;
     div.style.background = `${getRandomHexColor()}`;
     arrayBoxes.push(div);
   }
-  return arrayBoxes;
-}; 
+  return boxes.append(...arrayBoxes);
+};
+
 const destroyBoxes = () => {
   boxes.innerHTML = '';
 };
-createBtn.addEventListener('click', () => {
-  let boxesToAdd = createBoxes(inputBtn.value);
-  boxes.append(...boxesToAdd);
-});
+
 destroyBtn.addEventListener('click', () => {
   destroyBoxes();
 });
